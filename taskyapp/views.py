ae6197857500e3672c33341fbb9048f856d84afe
mohamed talaat <mohamed.talaat.info@gmail.com>
2026-02-19 08:37:33 +0200

    commit header

--- taskyapp/views.py
+++ taskyapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.views import View
 # Create your views here.
+
+
+class IndexView(View):
+    def get(self, request):
+        return render(request, "taskyapp/index.html")
+

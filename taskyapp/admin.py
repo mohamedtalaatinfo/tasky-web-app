bad27c884dbac315e4b6f2ba18af828bcd1e6cad
mohamed talaat <mohamed.talaat.info@gmail.com>
2026-02-19 09:38:06 +0200

    commit TaskyModel + admin

--- taskyapp/admin.py
+++ taskyapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import TaskyModel
 # Register your models here.
+
+
+class TaskyModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date', 'priority', 'task', 'complished']
+    list_filter = ['date', 'priority', 'complished']
+
+    
+
+admin.site.register(TaskyModel, TaskyModelAdmin)
--- taskyapp/models.py
+++ taskyapp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 # Create your models here.
+
+
+class TaskyModel(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now=True)
+    priority = models.IntegerField(validators=[
+      MinValueValidator(1),
+      MaxValueValidator(5)  
+    ])
+
+    task = models.CharField(max_length=200)
+    complished = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.priority} - {self.complished}"
